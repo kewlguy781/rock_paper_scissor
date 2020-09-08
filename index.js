9e6b645ac6ae0e2529237123c1eebb9ced0f9742
Jon Roberts <kewlguy@gmail.com>
2020-09-07 22:15:56 -0600

    all logistic is pretty much in

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let state = {
     winner: false,
     loser: false,
     tie: false,
+    winnercounter: "0",
+    losercounter: "0",
+    drawcounter: "0",
 };
 
 // Setting up HTML Strings input
@@ -16,11 +19,17 @@ function getMessage(){
 console.log('state.winner :>> ', state.winner);
     const {winner, loser, tie} = state;
     if (winner) {
-        return `You have won! <br> [Play Again]<br>`
+        state.winner = false
+        state.winnercounter++
+        return `You have won! <br> <br>`
     } else if (loser){
-        return `you lost!<br> [Play Again]<br>`
+        state.loser = false
+        state.losercounter++
+        return `you lost!<br><br>`
     } else if (tie){
-        return `Tie! The computer selected the same weapon as you do! [Play Again]`
+        state.tie = false
+        state.drawcounter++
+        return `Tie! The computer selected the same weapon as you do!<br><br>`
     } else {
         return `Please select your weapon!`
     }
@@ -43,7 +52,6 @@ function weaponSelected(index){
     //Checking to make sure its being clicked and returned with index
     computerindex = computerSelect()
     compareWeapons(index,computerindex)
-    //Need to figure out how to pass and print the choice and winner
 
 }
 
@@ -57,23 +65,38 @@ function compareWeapons(playeri,computeri){
 console.log('playeri :>> ', playeri);
 console.log('computeri :>> ', computeri);
 
-if (playeri === computeri) {
-    state.tie = true
-    render();
-} else if ((playeri == '0' && computeri == '2')|| 
-(playeri == '1' && computeri == '0')||
-(playeri == '2' && computeri == '1')) {
-    state.winner = true
-    render();
-} else {
-    state.loser=true
-    render();
-}
+    if (playeri === computeri) {
+        state.tie = true
+        render();
+    } else if ((playeri == '0' && computeri == '2')|| 
+    (playeri == '1' && computeri == '0')||
+    (playeri == '2' && computeri == '1')) {
+        state.winner = true
+        render();
+    } else {
+        state.loser=true
+        render();
+    }
 }
 
 // Winner Counter
+// See in State and the compare
 
 // Restart game (Clear array?)
+// Already in the "compare" where once result is determinated, reset the flag.
+
+// Result
+function renderResult(){
+    let resultHtml=`The computer select: <br><br>
+        the player select: 
+        
+        Winner counter: 
+        Player: ${state.winnercounter}
+        Computer: ${state.losercounter}
+        Draw: ${state.drawcounter}`;
+
+    return resultHtml;
+}
 
 // Render
 function render() {
@@ -83,7 +106,8 @@ function render() {
         ${getMessage()}
         <div class="main">
         ${renderWeapons()}
-        </div>
+        </div><br><br>
+        ${renderResult()}<br>
         </div>`;
 
     document.getElementById("app").innerHTML=htmlString;
